9d5490731ba6f704dc4d7f3bf51f9e2900e51115
player21o <196048914+player21o@users.noreply.github.com>
2025-02-17 14:55:44 +0100

    added basic chart to home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,24 @@ import HomeCard from "./HomeCard";
 import { UserContext } from "../contexts/UserContext";
 import useGetUser from "../hooks/GetUser";
 import { UserPermission } from "../protocol/packets";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip
+  //Legend
+);
 
 const Home = () => {
   const {
@@ -30,7 +48,35 @@ const Home = () => {
           />
           <h3>Недавние действия</h3>
         </HomeCard>
-        <HomeCard text="Статистика"></HomeCard>
+        <HomeCard text="Статистика">
+          <h3>За неделю</h3>
+          <Bar
+            data={{
+              labels: [...Array(7).keys()]
+                .reverse()
+                .map((d) =>
+                  new Date(
+                    new Date().setDate(new Date().getDate() - d)
+                  ).toLocaleDateString("ru-RU")
+                ),
+              datasets: [
+                {
+                  label: "Переведённые карточки",
+                  backgroundColor: "green",
+                  data: [1, 2, 3, 4, 5, 6, 70],
+                },
+              ],
+            }}
+            options={{
+              scales: { x: { ticks: { display: true, autoSkip: false } } },
+              responsive: true,
+              plugins: {
+                title: { display: false, text: "Переведённые карточки" },
+              },
+            }}
+            height={200}
+          />
+        </HomeCard>
       </div>
     </>
   ) : null;
