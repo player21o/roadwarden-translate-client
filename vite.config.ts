5a4f55d67c5c741c4ed0ed1ec5bc424ef5fac161
player21o <196048914+player21o@users.noreply.github.com>
2025-05-31 15:05:55 +0200

    upgraded to react 19. use of react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+    nodePolyfills(),
+  ],
   build: {
     rollupOptions: {
       output: {
